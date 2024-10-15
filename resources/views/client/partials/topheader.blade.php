c8c4fecc7d4e4dac8ee3c5bc7753ecc447741c0a
lucnt09 <nguyentienluc1924@gmail.com>
2024-10-15 20:48:41 +0700

    Cắt giao diện client

---
+++ resources/views/client/partials/topheader.blade.php
@@ -0,0 +1,68 @@
+<div class="header-top-nav">
+    <div class="container-fluid">
+        <div class="row align-items-center">
+            <!--Left Start-->
+            <div class="col-lg-4 col-md-12">
+                <div class="text-lg-start text-center">
+                    <p class="color-black">Welcome you to Ecolife store!</p>
+                </div>
+            </div>
+            <!--Left End-->
+            <!--Right Start-->
+            <div class="col-8 d-lg-block d-none">
+                <div class="header-right-nav">
+                    <ul>
+                        <li>
+                            <a href="compare.html"><i class="ion-ios-shuffle-strong"></i>Compare (0)</a>
+                        </li>
+                        <li class="border-color-black">
+                            <a href="wishlist.html"><i class="ion-android-favorite-outline"></i>Wishlist
+                                (0)</a>
+                        </li>
+                    </ul>
+                    <!-- Header Top Language Currency -->
+                    <div class="header-top-set-lan-curr d-flex justify-content-end">
+                        <div class="header-bottom-set dropdown">
+                            <button
+                                class="dropdown-toggle header-action-btn hover-style-default color-black border-color-black"
+                                data-bs-toggle="dropdown"> Settings <i
+                                    class="ion-ios-arrow-down"></i></button>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="my-account.html">My account</a></li>
+                                <li><a class="dropdown-item" href="checkout.html">Checkout</a></li>
+                                <li><a class="dropdown-item" href="login.html">Sign in</a></li>
+                            </ul>
+                        </div>
+                        <!-- Single Wedge Start -->
+                        <div class="header-top-curr dropdown">
+                            <button
+                                class="dropdown-toggle header-action-btn hover-style-default color-black border-color-black"
+                                data-bs-toggle="dropdown"><img class="me-2" src="{{ asset('client/assets/images/icons/1.jpg') }}" alt="">English<i class="ion-ios-arrow-down"></i></button>
+                            <ul class="dropdown-menu dropdown-menu-right">
+                                <li><a class="dropdown-item" href="#"><img src="{{ asset('client/assets/images/icons/1.jpg') }}"
+                                            alt="">English</a></li>
+                                <li><a class="dropdown-item" href="#"><img src="{{ asset('client/assets/images/icons/2.jpg') }}"
+                                            alt="">Français</a></li>
+                            </ul>
+                        </div>
+                        <!-- Single Wedge End -->
+                        <!-- Single Wedge Start -->
+                        <div class="header-top-curr dropdown">
+                            <button
+                                class="dropdown-toggle header-action-btn hover-style-default color-black border-color-black pr-0"
+                                data-bs-toggle="dropdown">USD $
+                                <i class="ion-ios-arrow-down"></i></button>
+                            <ul class="dropdown-menu dropdown-menu-right">
+                                <li><a class="dropdown-item" href="#">USD $</a></li>
+                                <li><a class="dropdown-item" href="#">EUR €</a></li>
+                            </ul>
+                        </div>
+                        <!-- Single Wedge End -->
+                    </div>
+                    <!-- Header Top Language Currency -->
+                </div>
+            </div>
+            <!--Right End-->
+        </div>
+    </div>
+</div>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,56 @@
+@extends('client.layouts')
+
+@section('title')
+    Trang Chủ
+@endsection
+
+@section('content')
+    <!-- Slider Arae Start -->
+    <div class="slider-area">
+        <!-- Slider Single Item Start -->
+        @include('client.partials.slider')
+        <!-- Slider Single Item End -->
+    </div>
+    <!-- Slider Arae End -->
+    <!-- Banner Area Start -->
+    <div class="banner-3-area">
+        @include('client.partials.banner')
+    </div>
+    <!-- Banner Area End -->
+    <!-- Static Area Start -->
+    <section class="static-area mtb-60px">
+        @include('client.partials.static')
+    </section>
+    <!-- Static Area End -->
+    <!-- Best Sells Area Start -->
+    <section class="best-sells-area">
+        @include('client.partials.bestseller')
+    </section>
+    <!-- Best Sell Area End -->
+    <!-- Feature Area 2 Start -->
+    <section class="feature-area-2">
+        @include('client.partials.feature')
+    </section>
+    <!-- Feature area 2 End -->
+
+    <!-- Hot deal area Start -->
+    <section class="hot-deal-area">
+        @include('client.partials.hotdeal')
+    </section>
+    <!-- Hot Deal Area End -->
+    <!-- Recent Add Product Area Start -->
+    <section class="recent-add-area">
+        @include('client.partials.newproduct')
+    </section>
+    <!-- Recent product area end -->
+    <!-- Blog area Start -->
+    <section class="blog-area mb-30px">
+        @include('client.partials.blog')
+    </section>
+    <!-- Blog Area End -->
+    <!-- Category Area Start -->
+    <section class="categorie-area">
+        @include('client.partials.categories')
+    </section>
+    <!-- Category Area End -->
+@endsection
