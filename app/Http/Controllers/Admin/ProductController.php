819d790713cd73a6cd097e90fd76a96f3666dc02
FeiXiong2004 <hunghn2004@gmail.com>
2024-10-25 20:56:28 +0700

    Update   UpdateProductResquest

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -10,32 +10,20 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    public function index(){
+    public function index()
+    {
 
         
         return view('admin.products.index');
-        
     }
-    public function create(){
-        
+    public function create()
+    {
+
         return view('admin.products.create');
     }
-    public function store(StoreProductRequest $request){
-        $data = $request->all();
-        Product::create($data);
-        return redirect()->route('admin.products.index')->with('message','Create successfully');
-    }
+    public function store(StoreProductRequest $request) {}
 
-    public function update(UpdateProductRequest $request, $id)
-{
-    $product = Product::findOrFail($id);
-
-    // Truyền sản phẩm hiện tại vào request
-    $request->setCurrentProduct($product);
-
-    // Nếu không có lỗi xác thực, tiến hành cập nhật
-    $product->update($request->validated());
-
-    return redirect()->route('products.index')->with('success', 'Product updated successfully');
-}
+    public function edit($id) {}
+    public function update(UpdateProductRequest $request, $id) {}
+    public function destroy($id) {}
 }
