60dbcf35ff283efc902eb1db3504d60de643a635
AyDiiCyyy <thanhdat32805@gmail.com>
2024-11-22 16:38:58 +0700

    commit v1 gio hang

--- resources/views/admin/blocks/siderbar.blade.php
+++ resources/views/admin/blocks/siderbar.blade.php
@@ -34,17 +34,16 @@
                         </p>
                     </a>
                     <ul class="nav nav-treeview">
-                        <li class="nav-item"> <a href="{{ route('admin.product.create') }}" class="nav-link"> <i
-                                    class="nav-icon bi bi-circle"></i>
-                                <p>Thêm mới</p>
-                            </a>
-                        </li>
                         <li class="nav-item"> <a href="{{ route('admin.product.index') }}" class="nav-link"> <i
                                     class="nav-icon bi bi-circle"></i>
                                 <p>Danh sách</p>
                             </a>
                         </li>
-
+                        <li class="nav-item"> <a href="{{ route('admin.product.create') }}" class="nav-link"> <i
+                                    class="nav-icon bi bi-circle"></i>
+                                <p>Thêm mới</p>
+                            </a>
+                        </li>
                     </ul>
                 </li>
                 <li class="nav-item">
--- resources/views/client/partials/head.blade.php
+++ resources/views/client/partials/head.blade.php
@@ -3,7 +3,7 @@
 <meta http-equiv="X-UA-Compatible" content="ie=edge" />
 <title>@yield('title')</title>
 <!-- Favicon -->
-<link rel="shortcut icon" type="image/x-icon" href="assets/images/favicon/favicon.png" />
+<link rel="shortcut icon" type="image/x-icon" href="{{ asset('client/assets/images/favicon/favicon.png') }}" />
 <!-- Google Fonts -->
 <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800&amp;display=swap" rel="stylesheet" />
 
