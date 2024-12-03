592a04ef30712f159e0a1d87e2d582d5f1a2f0aa
FeiXiong2004 <hunghn2004@gmail.com>
2024-12-03 09:57:35 +0700

    Fix  Client  SortByCategories

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -106,7 +106,7 @@
         integrity="sha256-XPpPaZlU8S/HWf7FZLAncLg2SAkP8ScUTII89x9D3lY=" crossorigin="anonymous"></script> <!-- jsvectormap -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"
         integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g=="
-        crossorigin="anonymous" referrerpolicy="no-referrer"></sc> {{-- link ajax --}}
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script> {{-- link ajax --}}
         <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>{{-- link thông báo --}}
         <script type="module">
             @if (session('status_succeed'))
