e250b5cdbdc8c48d8d5dc82d6a3ce8f9ee0c0d06
AyDiiCyyy <thanhdat32805@gmail.com>
2024-12-05 10:30:26 +0700

    fix thanh toán

--- app/Http/Controllers/Client/CheckoutController.php
+++ app/Http/Controllers/Client/CheckoutController.php
@@ -206,6 +206,9 @@ class CheckoutController extends Controller
             $user = Auth::user();
             $sum = 0;
             $products = $user->Carts;
+            if ($products->isEmpty()) {
+                return response()->json(['status' => 'error', 'message' => 'Đã xảy ra lỗi trong quá trình mua hàng']);
+            }
             foreach ($products as $product) {
                 $sum += $product->products->price * $product->quantity;
             }  // tính giá tổng của giỏ hàng hiện tại
