2f8ad6d1a480c7374f861e213107694628f1f60c
FeiXiong2004 <hunghn2004@gmail.com>
2024-12-06 10:56:00 +0700

    Remove netProfit add registerUser Statistic

--- resources/views/admin/stats.blade.php
+++ resources/views/admin/stats.blade.php
@@ -60,22 +60,23 @@
                             </svg>
                         </div> <!--end::Small Box Widget 4-->
                     </div>
-                    {{-- Lợi nhuận thuần  --}}
+                    {{-- Người dùng đăng ký  --}}
                     <div class="col-lg-4 col-6">
                         <div class="small-box text-bg-primary text-black">
                             <div class="inner">
-                                <h3 id="netProfit"></h3>
-                                <p class="fw-bold" id="netProfitLabel"></p>
+                                <h3 id="registerUser"></h3>
+                                <p class="fw-bold" id="registerUserLabel"></p>
                             </div>
                             <svg class="small-box-icon" fill="#1976D2" viewBox="0 0 24 24"
                                 xmlns="http://www.w3.org/2000/svg" aria-hidden="true"
                                 style="background-color: #BBDEFB; border-radius: 50%; padding: 4px;">
                                 <path
-                                    d="M2.25 2.25a.75.75 0 000 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 00-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 000-1.5H5.378A2.25 2.25 0 017.5 15h11.218a.75.75 0 00.674-.421 60.358 60.358 0 002.96-7.228.75.75 0 00-.525-.965A60.864 60.864 0 005.68 4.509l-.232-.867A1.875 1.875 0 003.636 2.25H2.25zM3.75 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zM16.5 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0z">
+                                    d="M6.25 6.375a4.125 4.125 0 118.25 0 4.125 4.125 0 01-8.25 0zM3.25 19.125a7.125 7.125 0 0114.25 0v.003l-.001.119a.75.75 0 01-.363.63 13.067 13.067 0 01-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 01-.364-.63l-.001-.122zM19.75 7.5a.75.75 0 00-1.5 0v2.25H16a.75.75 0 000 1.5h2.25v2.25a.75.75 0 001.5 0v-2.25H22a.75.75 0 000-1.5h-2.25V7.5z">
                                 </path>
+
+
+
                             </svg>
-
-
                         </div> <!--end::Small Box Widget 1-->
                     </div>
                     {{-- Doanh thu theo lọc --}}
@@ -218,14 +219,14 @@
         $(document).ready(function() {
             const defaultLabels = @json($labels);
             const defaultRevenue = @json($revenue);
-            const defaultNetProfitRevenue = @json($netProfitRevenue);
+            const defaultRegisterUser = @json($registerUser);
             const defaultPendingOrder = @json($pendingOrder);
             const defaultOrderProcessing = @json($orderProcessing);
             const defaultCancelConfirm = @json($cancelConfirm);
             const defaultBestSellerTop10 = @json($bestSellerTop10);
             const defaultCompletedOrder = @json($completedOrder);
             const defaultCancelOrder = @json($cancelOrder);
-        
+
             // Hiển thị mặc định khi chưa lọc
             if (defaultLabels && defaultRevenue) {
                 updateBarChart({
@@ -233,7 +234,7 @@
                     revenue: defaultRevenue
                 });
                 updateRevenueInfo(defaultRevenue, 'Doanh thu 14 ngày gần nhất');
-                updateNetProfit(defaultNetProfitRevenue, 'Lợi nhuận thuần');
+                updateRegisterUser(defaultRegisterUser, 'Người dùng đăng ký');
                 pendingOrder(defaultPendingOrder, 'Đơn hàng chờ xác nhận');
                 orderProcessing(defaultOrderProcessing, 'Đơn hàng đang xử lý')
                 cancelConfirm(defaultCancelConfirm, 'Đơn hàng chờ xác nhận hủy');
@@ -268,10 +269,10 @@
                                     labels: response.labels,
                                     revenue: response.revenue,
                                 });
-
+                                updateRevenueInfo(response.revenue, filterValue);
                             }
-                            if (response.netProfitRevenue !== undefined) {
-                                updateNetProfit(response.netProfitRevenue, filterValue);
+                            if (response.registerUser !== undefined) {
+                                updateRegisterUser(response.registerUser, filterValue);
                             }
                             if (response.pendingOrder !== undefined) {
                                 pendingOrder(response.pendingOrder, filterValue);
@@ -341,8 +342,8 @@
                                 });
                                 updateRevenueInfo(response.revenue, filter);
                             }
-                            if (response.netProfitRevenue !== undefined) {
-                                updateNetProfit(response.netProfitRevenue, filter);
+                            if (response.registerUser !== undefined) {
+                                updateRegisterUser(response.registerUser, filter);
                             }
                             if (response.pendingOrder !== undefined) {
                                 pendingOrder(response.pendingOrder, filter);
@@ -451,11 +452,11 @@
             // Cập nhật tổng doanh thu vào phần tử h3
             $('#revenueValue').text(totalRevenue.toLocaleString() + ' VNĐ');
         }
-        // Lợi nhuận thuần 
-        function updateNetProfit(netProfit, filter) {
-            if (!netProfit) {
-                $('#netProfitLabel').text('Lợi nhuận thuần');
-                $('#netProfit').text('0 VNĐ');
+        // Người dùng đăng ký 
+        function updateRegisterUser(registerUser, filter) {
+             // Kiểm tra xem countStatus có hợp lệ không
+             if (registerUser === undefined || registerUser === null) {
+                registerUser = 0; // Nếu không có dữ liệu, gán mặc định là 0
             }
 
             // Đặt nhãn dựa trên filter
@@ -463,23 +464,23 @@
 
             switch (filter) {
                 case 'day':
-                    label = 'Lợi nhuận thuần theo ngày';
+                    label = 'Người dùng đăng ký theo ngày';
                     break;
                 case 'month':
-                    label = 'Lợi nhuận thuần theo tháng';
+                    label = 'Người dùng đăng ký theo tháng';
                     break;
                 case 'year':
-                    label = 'Lợi nhuận thuần theo năm';
+                    label = 'Người dùng đăng ký theo năm';
                     break;
                 case '14day':
-                    label = 'Lợi nhuận thuần 14 ngày gần nhất';
+                    label = 'Người dùng đăng ký 14 ngày gần nhất';
                     break;
                 default:
-                    label = 'Lợi nhuận thuần';
+                    label = 'Người dùng đăng ký';
                     break;
             }
-            $('#netProfitLabel').text(label);
-            $('#netProfit').text(netProfit.toLocaleString() + ' VNĐ');
+            $('#registerUserLabel').text(label);
+            $('#registerUser').text(registerUser);
         }
         // Trạng thái đang xác nhận
         function pendingOrder(countStatus, filter) {
